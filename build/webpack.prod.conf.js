338da7438eae4d0bec6b80d7acc1f1f3b95a2e76
rpiontik <r.piontik@mail.ru>
2018-02-09 10:26:42 +0300

    Fix including modules Problem - not correct including inline js and css code to index.html

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -49,7 +49,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: utils.assetsPath('css/[name].css'),
       // Setting the following option to `false` will not extract CSS from codesplit chunks.
       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
+      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
       // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
       //allChunks: true,
     }),
@@ -70,9 +70,18 @@ const webpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: false,
       //Injection source code to HTML
-      app_js: fs.readFileSync('dist/static/js/app.js', 'utf8'),
-      app_css: fs.readFileSync('dist/static/css/app.css', 'utf8'),
-      vendor_js: fs.readFileSync('dist/static/js/vendor.js', 'utf8'),
+      app_js: function(){
+        console.error("app_js");
+        return fs.readFileSync('dist/static/js/app.js', 'utf8');
+      },
+      app_css: function(){
+        console.error("app_css");
+        return fs.readFileSync('dist/static/css/app.css', 'utf8');
+      },
+      vendor_js: function(){
+        console.error("vendor_js");
+        return fs.readFileSync('dist/static/js/vendor.js', 'utf8');
+      },
       minify: {
         removeComments: true,
         collapseWhitespace: true,
