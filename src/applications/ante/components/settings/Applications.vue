9627d8760dfe5a3a1bc37aad81811267c0b4d5af
RPiontik <roman@cryptonit.net>
2018-03-26 21:23:40 +0300

    Refactoring applications for new core

--- src/applications/ante/components/settings/Applications.vue
+++ src/applications/ante/components/settings/Applications.vue
@@ -5,7 +5,21 @@
                 <v-layout row wrap>
                     <h1>{{'APPS_TITLE' | lang}}</h1>
                     <v-flex xs12>
-                        Applications<br>Applications<br>Applications<br>Applications<br>Applications<br>Applications<br>Applications<br>Applications<br>Applications<br>Applications<br>Applications<br>
+                        <v-data-table
+                                :headers="headers"
+                                :items="items"
+                                hide-actions
+                                class="elevation-1"
+                        >
+                            <template slot="items" slot-scope="props">
+                                <td>{{ props.item.name }}</td>
+                                <td class="text-xs-right">{{ props.item.calories }}</td>
+                                <td class="text-xs-right">{{ props.item.fat }}</td>
+                                <td class="text-xs-right">{{ props.item.carbs }}</td>
+                                <td class="text-xs-right">{{ props.item.protein }}</td>
+                                <td class="text-xs-right">{{ props.item.iron }}</td>
+                            </template>
+                        </v-data-table>
                     </v-flex>
                 </v-layout>
             </v-card-title>
@@ -29,6 +43,49 @@
         },
         data () {
             return {
+                headers: [
+                    {
+                        text: 'Dessert (100g serving)',
+                        align: 'left',
+                        sortable: false,
+                        value: 'name'
+                    },
+                    { text: 'Calories', value: 'calories' },
+                    { text: 'Fat (g)', value: 'fat' },
+                    { text: 'Carbs (g)', value: 'carbs' },
+                    { text: 'Protein (g)', value: 'protein' },
+                    { text: 'Iron (%)', value: 'iron' }
+                ],
+                items: [
+                    {
+                        value: false,
+                        name: 'Frozen Yogurt',
+                        calories: 159,
+                        fat: 6.0,
+                        carbs: 24,
+                        protein: 4.0,
+                        iron: '1%'
+                    },
+                    {
+                        value: false,
+                        name: 'Ice cream sandwich',
+                        calories: 237,
+                        fat: 9.0,
+                        carbs: 37,
+                        protein: 4.3,
+                        iron: '1%'
+                    },
+                    {
+                        value: false,
+                        name: 'Eclair',
+                        calories: 262,
+                        fat: 16.0,
+                        carbs: 23,
+                        protein: 6.0,
+                        iron: '7%'
+                    },
+
+                ]
             }
         }
     }
