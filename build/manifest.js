756a9a676bd5c44c83f6d40d050b1aa62eea3781
RPiontik <roman@cryptonit.net>
2018-04-16 19:41:25 +0300

    Storage developing

--- build/manifest.js
+++ build/manifest.js
@@ -7,14 +7,42 @@ module.exports = {
     BIN_BLOCK_ENTRY : 1,
     BIN_BLOCK_SUBSCRIPTION : 2,
     check(manifest){
-       //todo application name max 64
-       //todo file name max 64
-       //todo MAX_TASK_NAME_LEN 16
+        //todo file name max 64
+        //todo MAX_TASK_NAME_LEN 16
+        if(!('name' in manifest))
+            throw new Error(`Unknown application name`);
+
+        if(manifest.name.length > 64)
+            throw new Error(`Application name is too long ${manifest.name} max 64 chars`);
+
+        if('scripts' in manifest){
+            if(!('entry' in manifest.scripts))
+                throw new Error(`The ${manifest.name} application do not have required block [entry] im manifest`);
+
+            if(!('modules' in manifest.scripts))
+                throw new Error(`The ${manifest.name} application do not have required block [modules] in manifest`);
+
+            if(!(manifest.scripts.entry in manifest.scripts.modules))
+                throw new Error(`The ${manifest.name} application has entry [${manifest.scripts.entry}] but do not have module for it`);
+        }
+
+        if('storage' in manifest) {
+            if(!('version' in manifest.storage))
+                throw new Error(`Storage of ${manifest.name} do not have required field "version"`);
+            if(!('migration' in manifest.storage))
+                throw new Error(`Storage of ${manifest.name} do not have required field "migration"`);
+            if(!('scripts' in manifest) || !('modules' in manifest.scripts) || !(manifest.storage.migration in manifest.scripts.modules))
+                throw new Error(`The ${manifest.name} application has migration [${manifest.storage.migration}] but do not have module for it`);
+            if(!('objects' in manifest.storage))
+                throw new Error(`Storage of ${manifest.name} do not have required block "objects"`);
+        }
     },
     make(app){
         const app_path  = path.resolve(__dirname, '../src/applications/', app);
 
         let manifest    = require(path.resolve(app_path, "manifest.json"));
+
+        this.check(manifest);
 
         if(fs.existsSync(path.resolve(app_path, "favicon.png"))) {
             manifest.favicon    = 'data:image/png;base64,'
@@ -37,6 +65,8 @@ module.exports = {
         return manifest;
     },
     binary(manifest){
+        this.check(manifest);
+
         //No script side
         if(!('scripts' in manifest))
             return Buffer.from([]);
@@ -46,18 +76,11 @@ module.exports = {
         result.push(Buffer.from(new Uint32Array([this.BIN_BLOCK_NAME, manifest.name.length]).buffer));
         result.push(Buffer.from(manifest.name, 'UTF-8'));
 
-        if(!('entry' in manifest.scripts))
-            throw new Error(`The ${manifest.name} application do not have required block [entry] im manifest`);
-
-        if(!('modules' in manifest.scripts))
-            throw new Error(`The ${manifest.name} application do not have required block [modules] in manifest`);
-
-        if(!(manifest.scripts.entry in manifest.scripts.modules))
-            throw new Error(`The ${manifest.name} application has entry [${manifest.scripts.entry}] but do not have module for it`);
-
+        //Entry name
         result.push(Buffer.from(new Uint32Array([this.BIN_BLOCK_ENTRY, manifest.scripts.entry.length]).buffer));
         result.push(Buffer.from(manifest.scripts.entry, 'UTF-8'));
 
+        //Subscriptions
         if('subscriptions' in manifest.scripts)
             manifest.scripts.subscriptions.map((item)=>{
                 result.push(Buffer.from(new Uint32Array([this.BIN_BLOCK_SUBSCRIPTION, item.length]).buffer));
