1b3b1ba29d01a699ab1add58d54d1d6d25ea06ae
RPiontik <roman@cryptonit.net>
2018-07-06 21:16:55 +0300

    Execution schedule

--- src/applications/lucerna/scripts/scheduler.js
+++ src/applications/lucerna/scripts/scheduler.js
@@ -3,60 +3,18 @@ let emit = ffi('void emit(char*, char*)');
 let log = ffi('void log(char*, char*, int)');
 log('MJS', 'Starting Lucerna script...', 1);
 
-/*
-let rec_max = 0;
-let record_num;
-let start = $core.time();
-
-let dots = $storage.open("dots");
-function append(max) {
-    let start = $core.time();
-    let n;
-    print(">>>> Started <<<<");
-    for (n = 0; n < max; n++) {
-        $storage.append(dots, {
-            "brightness": n,
-            "levels": {
-                "0": 10,
-                "1": 20,
-                "2": 30,
-                "3": 40,
-                "4": 50,
-                "5": 60,
-                "6": 70,
-                "7": 80,
-                "8": 90,
-                "9": 100,
-                "10": 90,
-                "11": 80,
-                "12": 70,
-                "13": 60,
-                "14": 50,
-                "15": 40
-            }
-        });
-        print("Append ", n, " record");
-    }
-    print("Append ", n, " records spent ", $core.time() - start, " sec");
-}
-
-append(rec_max);
-
-$storage.close(dots);
-*/
-
 function getConfig(){
     let config = $storage.open("config");
     let result = {
-        interval : {
-            width : 86400
+        "interval" : {
+            "width" : 86400
         },
-        channelNumber : 4,
-        channels : {
-            0  : 100000, 1  : 100000, 2  : 100000, 3  : 100000,
-            4  : 100000, 5  : 100000, 6  : 100000, 7  : 100000,
-            8  : 100000, 9  : 100000, 10 : 100000, 11 : 100000,
-            12 : 100000, 13 : 100000, 14 : 100000, 15 : 100000,
+        "channelNumber" : 4,
+        "channels" : {
+            "0"  : 100000, "1"  : 100000, "2"  : 100000, "3"  : 100000,
+            "4"  : 100000, "5"  : 100000, "6"  : 100000, "7"  : 100000,
+            "8"  : 100000, "9"  : 100000, "10" : 100000, "11" : 100000,
+            "12" : 100000, "13" : 100000, "14" : 100000, "15" : 100000,
         }
     };
     if($storage.first(config))
@@ -68,24 +26,57 @@ function getConfig(){
 let config = getConfig();
 
 function getCurrentInterval(){
+    let time = $core.time() % config.interval.width;
+    let prev_dot = null;
+    let next_dot = null;
+    let rec_scanned = 0;
+
+    print('Current time is ', time);
+
     let dots = $storage.open("dots");
-    let time = $core.time() %
     for(let found = $storage.first(dots); found ; found = $storage.next(dots)){
-        if()
         let dot = $storage.get(dots);
+        if(dot.time < time) {
+            prev_dot = dot;
+        } else {
+            break;
+        }
         print("Dot time=", dot.time, "brightness=", dot.brightness);
-        ++record_num;
+        ++rec_scanned;
     }
-    print("Total dots is ", record_num);
+
     $storage.close(dots);
+    print("Records scanned ", rec_scanned);
+
+    if(prev_dot) {
+        if($storage.next(dots)) {
+            next_dot = $storage.get(dots);
+        } else if($storage.first(dots)) {
+            next_dot = prev_dot;
+            prev_dot = $storage.get(dots);
+        }
+        print("Prev dot time=", prev_dot.time, " next dot time=", next_dot.time);
+        return {
+            start : prev_dot,
+            stop : next_dot
+        };
+    }
+
+    print("No first dot :(");
+    return null;
 }
 
 function doInterval(){
     print("Calculating interval....");
     let interval = getCurrentInterval();
-    print("Interval is ", interval.start, '<>', interval.stop);
+    if(interval) {
+        print("Interval is ", interval.start, '<>', interval.stop);
+    } else {
+        print("No interval");
+    }
 }
 
+doInterval();
 
 listener(function(event, content, data) {
     print(">>> EVENT: ", event, ";", content, ";", data, "<<<");
@@ -97,52 +88,3 @@ listener(function(event, content, data) {
 }, null);
 
 log('MJS', 'Lucerna script is stared.', 1);
-
-/*
-
-let update_start = $core.time();
-record_num = 0;
-for(let found = $storage.first(dots); found ; found = $storage.next(dots)){
-    let dot = $storage.get(dots);
-    if(record_num < (rec_max / 2)) {
-        $storage.remove(dots);
-    } else {
-        dot.levels["9"] += 10;
-        $storage.post(dots, dot);
-    }
-    ++record_num;
-}
-print("Updated ", record_num, " records. Spent ", $core.time() - update_start, " sec");
-
-let shrink_start = $core.time();
-$storage.shrink(dots);
-print("Shrinked to ", $storage.size(dots), ". Spent ", $core.time() - shrink_start, " sec");
-
-//append(rec_max);
-
-let select_start = $core.time();
-record_num = 0;
-for(let found = $storage.first(dots); found; found = $storage.next(dots)){
-    ++record_num;
-    let dot = $storage.get(dots);
-    //log('MJS', 'Record num ' + JSON.stringify(record_num) + ' dot.levels["9"] = ' + JSON.stringify(dot.levels["9"]), 1);
-}
-print("Selected ", record_num, " records. Spent ", $core.time() - select_start, " time file size ", $storage.size(dots));
-
-$storage.close(dots);
-
-print("Total work with storage ", $core.time() - start);
-
-listener(function(event, content, data) {
-    if(event === "$-started"){
-        log('MJS', 'STARTED!!!! test variable = ' + test, 1);   //Error info
-    } else if(event === "$-script-error") {
-        log('MJS', 'DETECT SCRIPT ERROR IN ' + content, 1);   //Error info
-    } else if(event === "lucerna-echo") {
-        log('MJS', 'FOR MAX: ' + content, 1);   //Error info
-        emit('lucerna-test', 'I recived:' + content);
-    } else {
-        log('MJS', 'EVENT [' +event + "]:[" + content + "]", 1);   //Error info
-    }
-}, null);
-*/
